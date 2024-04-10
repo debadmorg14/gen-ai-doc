39020b8a3f1a7775288b60b2574ba0e8f6208bc4
Deba <debadatta.moha@gmail.com>
2024-04-10 10:02:08 +0200

    workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: organization, // Usually your GitHub org/user name.
   projectName: projectName, // Usually your repo name.
-  // deploymentBranch: 'main',
+  deploymentBranch: 'main',
   githubHost: 'github.com',
 
 
