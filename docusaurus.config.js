e048b50d807f6a9abfa6534e9997f2abed5661e0
Deba <debadatta.moha@gmail.com>
2024-05-02 10:39:54 +0200

    blog part

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/my_project_social.jpg',
+      image: 'img/my_project_social.png',
       navbar: {
         title: 'Gen AI Developer Guide',
         logo: {
